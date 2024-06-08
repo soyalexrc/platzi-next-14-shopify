4b3aa6a2479d277b19ecdf9f5b9034beef6b72c7
alexrodriguez <alexcarvajal2404@gmail.com>
2024-06-08 16:00:57 -0500

    :zap: implementacion de estilos globales

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import { Header } from '@/components/shared/Header'
 import { Footer } from '@/components/shared/Footer'
+import  '@/sass/globals.sass';
 
 const inter = Inter({ subsets: ['latin'] })
 
