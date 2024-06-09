427a722f05bf4be0c58a0923e681fffdf9f81c05
alexrodriguez <alexcarvajal2404@gmail.com>
2024-06-09 11:21:20 -0500

    :zap: explorando optimizacion de imagenes en next js

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,19 @@
+import styles from './Description.module.sass';
+import Image from 'next/image';
+
 export const Description = () => {
-    return(
-        <section>
-            <h1>Descripción</h1>
+    return (
+        <section className={styles.Description}>
+            <Image
+                src="/images/description.jpeg"
+                alt="products marketplace"
+                width={500}
+                height={300}
+            />
+            <div className={styles.Description__text}>
+                <h2>Bring the future today</h2>
+                <p>Future World: Your Gateway to Tomorrow&apos;s Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
+            </div>
         </section>
     )
 }
