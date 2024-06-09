a1aa2cbc92df1f6d79d80c36583f372228d0e067
alexrodriguez <alexcarvajal2404@gmail.com>
2024-06-09 11:57:39 -0500

    :zap: configurando variables de entorno y primer test de fetch de datos hacia shopify store
    creacion y configuracion de tiensa de shopify mediante el shopify partners program

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,16 @@
-export const MainProducts = () => {
+const getProducts = async () => {
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
+        headers: new Headers({
+            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ''
+        })
+    })
+
+    return await response.json();
+}
+
+export const MainProducts = async () => {
+    const products = await getProducts();
+    console.log(products);
     return(
         <section>
             <h1>MainProducts</h1>
