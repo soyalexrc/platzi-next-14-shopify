bb125e58017fa2d1786d3ae63192b6de13f938ca
alexrodriguez <alexcarvajal2404@gmail.com>
2024-06-10 12:03:43 -0500

    :zap: redirect management

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,6 @@
 import {getProducts} from "@/services/shopify/products";
 import {ProductView} from "@/components/product/ProductView";
+import {redirect} from "next/navigation";
 
 interface PageProps {
     searchParams: {
@@ -11,6 +12,10 @@ export default async function Page({ searchParams }: PageProps) {
     const id = searchParams.id
     const products = await getProducts(id)
     const product = products[0]
+
+    if (!id) {
+        redirect('/')
+    }
 
     return <ProductView product={product} />
 }
