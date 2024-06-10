a6ddd0a5f355927011fcd95e0bf14275f6851959
alexrodriguez <alexcarvajal2404@gmail.com>
2024-06-10 12:18:45 -0500

    :zap: manejo de seo

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,11 @@
 import { MainProducts } from "@/components/home/MainProducts"
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Future World",
+    description: "Welcome to the future world, an ecommerce form other century",
+    keywords: ['ecommerce', 'future', 'world', 'technology'],
+}
 
 export default function Home() {
     return (
--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -8,6 +8,23 @@ interface PageProps {
     }
 }
 
+export async function generateMetadata({ searchParams }: PageProps) {
+    const id = searchParams.id
+    const products = await getProducts(id)
+    const product = products[0]
+
+    return {
+        title: product.title,
+        description: product.description,
+        keywords: product.tags,
+        openGraph: {
+            images: [
+                product.image
+            ]
+        }
+    }
+}
+
 export default async function Page({ searchParams }: PageProps) {
     const id = searchParams.id
     const products = await getProducts(id)
