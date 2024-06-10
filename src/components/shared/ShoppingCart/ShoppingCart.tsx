67c187c235495249a6a8fbee7b18278411fa1eee
alexrodriguez <alexcarvajal2404@gmail.com>
2024-06-10 18:25:10 -0500

    :zap: implementacion de funcionalidad de carrito de compras con zustand

--- src/components/shared/ShoppingCart/ShoppingCart.tsx
+++ src/components/shared/ShoppingCart/ShoppingCart.tsx
@@ -1,17 +1,17 @@
 "use client";
-import { useState } from "react";
-import { FaShoppingCart } from "react-icons/fa";
+import {FaShoppingCart} from "react-icons/fa";
 import styles from './ShoppingCart.module.sass'
+import {useShoppingCart} from "@/hooks/useShoppingCart";
 
 export const ShoppingCart = () => {
-    const [countItems, setCountItems] = useState(0);
+    const {cart} = useShoppingCart();
 
     return (
         <button className={styles.ShoppingCart}>
       <span className={styles.ShoppingCart__counter}>
-        {countItems}
+        {cart.length}
       </span>
-            <FaShoppingCart />
+            <FaShoppingCart/>
         </button>
     )
 }
