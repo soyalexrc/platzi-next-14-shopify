7b73fdd84d582d8f54a5b418a2025d86bf857088
alexrodriguez <alexcarvajal2404@gmail.com>
2024-06-10 16:47:49 -0500

    :zap: signup process with graphql and storefront api shopify

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,20 @@
 "use server"
+import { GraphQLClientSingleton } from "@/graphql"
+import { createUserMutation } from "@/graphql/mutations/createUserMutation"
 
-export const handleCreateUser = async (formData: any) => {
-    console.log("handleCreateUser", formData)
+export const handleCreateUser = async (formData: FormData) => {
+    const formDataObject = Object.fromEntries(formData)
+    delete formDataObject["password_confirmation"]
+    const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+    const variables = {
+        input: {
+            ...formDataObject,
+            phone: '+51' + formDataObject.phone
+        }
+    }
+
+    const { customerCreate }: { customerCreate: any} = await graphqlClient.request(createUserMutation, variables)
+    const { customerUserErrors, customer } = customerCreate
+    console.log(customer)
+    console.log(customerUserErrors)
 }
--- src/config/env.ts
+++ src/config/env.ts
@@ -2,10 +2,14 @@ export const env: ENV = {
     SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY || '',
     SHOPIFY_HOSTNAME: process.env.SHOPIFY_HOSTNAME || '',
     CACHE_TOKEN: process.env.CACHE_TOKEN || '',
+    SHOPIFY_GRAPHQL_ENDPOINT: process.env.SHOPIFY_GRAPHQL_ENDPOINT || '',
+    SHOPIFY_STOREFRONT_TOKEN: process.env.SHOPIFY_STOREFRONT_TOKEN || '',
 }
 
 type ENV = {
     SHOPIFY_API_KEY: string;
     SHOPIFY_HOSTNAME: string;
     CACHE_TOKEN: string;
+    SHOPIFY_GRAPHQL_ENDPOINT: string;
+    SHOPIFY_STOREFRONT_TOKEN: string;
 }
